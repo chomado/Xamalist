d3b0a92b4c596961758281701cec3142f9ab9f2d
chomado <chomado@gmail.com>
2016-12-05 19:09:50 +0900

    詳細ページ(空っぽ)を追加

---
+++ Xamalist/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xamalist.ViewModels
+{
+	public class DetailPageViewModel
+	{
+		public DetailPageViewModel()
+		{
+		}
+	}
+}
---
+++ Xamalist/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamalist
+{
+	public partial class DetailPage : ContentPage
+	{
+		public DetailPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
