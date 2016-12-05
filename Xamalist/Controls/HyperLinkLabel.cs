dba985f2e9ec8d597751a8b77827c61a632c6643
chomado <chomado@gmail.com>
2016-12-05 21:39:52 +0900

    さっき作ったカスタムコントロールのリンク文字列を変更できるようにした＆文字色変更

--- Xamalist/Controls/HyperLinkLabel.cs
+++ Xamalist/Controls/HyperLinkLabel.cs
@@ -8,11 +8,30 @@ namespace Xamalist.Controls
 	{
 		public HyperLinkLabel()
 		{
+			// 文字色を変える
+			this.TextColor = Color.Gray;
+
+			// タップした時に URLを開く、という処理を書いている
 			var tapGestureRecognizer = new TapGestureRecognizer();
 			tapGestureRecognizer.Tapped += (sender, e) => {
-				Device.OpenUri(new Uri(((Label)sender).Text));
+				Device.OpenUri(new Uri(((HyperLinkLabel)sender).Url));
 			};
 			this.GestureRecognizers.Add(tapGestureRecognizer);
 		}
+
+		// "Url" という名前の BindableProperty を定義
+		public static readonly BindableProperty UrlProperty = BindableProperty.Create(
+			propertyName: "URL",
+			returnType: typeof(string),
+			declaringType: typeof(HyperLinkLabel),
+			defaultValue: null
+		);
+
+		// BindableProperty の CLRプロパティの wrapper
+		public string Url { 
+			get { return (string)GetValue(UrlProperty); }
+			set { SetValue(UrlProperty, value); }
+		}
+
 	}
 }
