3e862a96593791e3f8e261fc2810518118905324
chomado <chomado@gmail.com>
2016-12-05 21:15:13 +0900

    リンクの機能の持つラベルである、カスタムコントロールを作った！

---
+++ Xamalist/Controls/HyperLinkLabel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Xamalist.Controls
+{
+	// タップしたらテキストの中身を開く、という機能を持つラベル
+	public class HyperLinkLabel : Label
+	{
+		public HyperLinkLabel()
+		{
+			var tapGestureRecognizer = new TapGestureRecognizer();
+			tapGestureRecognizer.Tapped += (sender, e) => {
+				Device.OpenUri(new Uri(((Label)sender).Text));
+			};
+			this.GestureRecognizers.Add(tapGestureRecognizer);
+		}
+	}
+}
