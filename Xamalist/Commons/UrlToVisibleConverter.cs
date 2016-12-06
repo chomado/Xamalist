e01d47e9ae96e736bc4dd97cf5d704d432fb16ea
chomado <chomado@gmail.com>
2016-12-06 21:48:04 +0900

    GitHubのURLがあるか無いかでアイコン出し分け

---
+++ Xamalist/Commons/UrlToVisibleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+namespace Xamalist.Commons
+{
+    public class UrlToVisibleConverter : IValueConverter
+    {
+        public UrlToVisibleConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // GitHub の URL が空だったら false、存在していたら true を返す
+            var gitHubUrl = (string)value;
+            return !string.IsNullOrEmpty(gitHubUrl);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
