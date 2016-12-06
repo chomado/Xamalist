6a9a23d25ab04e1f17520924ffcba35172a7999c
chomado <chomado@gmail.com>
2016-12-06 21:40:22 +0900

    対応プラットフォームごとにアイコン出し分け処理（コンバーターを書いた）

---
+++ Xamalist/Commons/IconEnableDisableConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Xamalist.Commons
+{
+	public class IconEnableDisableConverter : IValueConverter
+	{
+		public IconEnableDisableConverter()
+		{
+		}
+
+		// ViewModel から画面に行くときの変換処理
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var osName = (string)parameter;
+			var storeUrl = (string)value;
+
+			// ストアのURLが空だったら
+			if (string.IsNullOrEmpty(storeUrl))
+			{
+				return $"{osName}Disable.png";
+			}
+			return $"{osName}Enable.png";
+		}
+
+		// 画面から ViewModel に行くときの変換処理
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
