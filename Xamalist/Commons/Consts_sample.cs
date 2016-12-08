8cc85dc043dbad51ad8aaeab7652d53de2e6c0e7
chomado <chomado@gmail.com>
2016-12-08 19:36:07 +0900

    Constsファイルのサンプルを置いた。

---
+++ Xamalist/Commons/Consts_sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Xamalist
+{
+    // 「Consts」って rename して使ってください
+    public static class Consts_sample
+    {
+        // 接続先の Webサーバのアドレス
+        public static readonly string AzureWebsitesUrl = "https://(サーバーのURL).azurewebsites.net";
+
+        // Azure 上のストレージの接続文字列
+        public static readonly string StorageConnectionString = "めっちゃ長い接続文字列。Azureのポータルの「アクセスキー」から取って来てね";
+
+    }
+}
