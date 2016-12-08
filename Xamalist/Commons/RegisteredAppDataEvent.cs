917d4b0f41a090af15673a4aeb451cb56e9ea8a3
chomado <chomado@gmail.com>
2016-12-08 17:29:04 +0900

    名前空間の変更

--- Xamalist/Commons/RegisteredAppDataEvent.cs
+++ Xamalist/Commons/RegisteredAppDataEvent.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Prism.Events;
 
-namespace Xamalist
+namespace Xamalist.Commons
 {
     // 「登録完了イベントだよ」っていう目印のために定義する必要のあったクラス
     public class RegisteredAppDataEvent : PubSubEvent
