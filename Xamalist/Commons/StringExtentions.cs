1422a3eb2a80bba53b43afa3ea8afdb5d689cf55
chomado <chomado@gmail.com>
2016-12-08 17:18:05 +0900

    string.IsNullOrEmpty の拡張メソッドを作っておいた

---
+++ Xamalist/Commons/StringExtentions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Xamalist.Commons
+{
+    public static class StringExtentions
+    {
+        public static bool IsNullOrEmpty(this string self)
+        {
+            return string.IsNullOrEmpty(self);
+        }
+    }
+}
