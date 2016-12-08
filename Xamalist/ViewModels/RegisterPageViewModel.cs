95b2f8a00557c0765c54da84138e16c49125dd38
chomado <chomado@gmail.com>
2016-12-08 16:10:49 +0900

    DBに「書き込み」中はスピナー表示＆「登録」ボタン非活性

--- Xamalist/ViewModels/RegisterPageViewModel.cs
+++ Xamalist/ViewModels/RegisterPageViewModel.cs
@@ -9,22 +9,33 @@ using System.Threading.Tasks;
 namespace Xamalist.ViewModels
 {
     public class RegisterPageViewModel : BindableBase
-	{
+    {
         public RegisterPageViewModel(IAppDataService appDataService)
-		{
+        {
             this.appDataService = appDataService;
-
 
             // 登録ボタンが押された時のコマンドを定義
             this.RegisterCommand = new DelegateCommand(
-                executeMethod: async () => await this.appDataService.InsertAppDataAsync(this.RegisteringAppData)
-            );
-		}
+                executeMethod:    async () => await this.RegisterAppDataAsync(),
+                canExecuteMethod: () => !this.IsBusy /* コマンドが実行可能のときに true を返す */
+            )
+            .ObservesProperty(propertyExpression: () => this.IsBusy); // IsBusyプロパティを監視し、IsBusyに変化があったら、CanExecuteChangedイベントを発行する
+        }
 
         private IAppDataService appDataService;
 
         // 登録ボタン
         public DelegateCommand RegisterCommand { get; }
+
+
+        // データ読み込み中などにスピナーを表示するために使う値
+        private bool isBusy;
+        public bool IsBusy
+        {
+            get { return this.isBusy; }
+            set { SetProperty(ref this.isBusy, value); }
+        }
+
 
         // --- 新規登録する時にデータを入れる入れ物 ----
         private AppData registeringAppData = new AppData();
@@ -34,6 +45,17 @@ namespace Xamalist.ViewModels
             set { SetProperty(ref this.registeringAppData, value); }
         }
 
-
+        private async Task RegisterAppDataAsync()
+        {
+            /* 登録ボタンが押される
+             * → ぐるぐる ＆ ボタン非アクティブ
+             * → 「登録されました！」トースト
+             * → 前の画面(MainPage)に自動遷移
+             * → 勝手にリストが「更新」される
+             */
+            this.IsBusy = true;
+            await this.appDataService.InsertAppDataAsync(this.RegisteringAppData);
+            this.IsBusy = false;
+        }
 	}
 }
